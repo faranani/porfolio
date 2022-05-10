c9d3b92f016110e46ee1c87b3b181e6dfc5434f0
faranani <faranani.ramakhetha@dadevs.co.za>
2022-05-10 16:21:49 +0200

    about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'About', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  {
+    path:'', redirectTo:'home',pathMatch:'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
